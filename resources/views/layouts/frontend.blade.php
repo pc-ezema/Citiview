6e94a3081de3e0cf5ccf71e5fb837d831357e7a3
Promise <promiseezema11@gmail.com>
2024-10-12 12:28:15 +0100

    updated

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>Hotel Alpha - Booking and Reservation Template</title>
+    <title>CiTiVIEW Hotel - Booking and Reservation</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
